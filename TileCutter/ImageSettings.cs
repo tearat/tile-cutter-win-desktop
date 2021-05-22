16d6dbf59e42bf2c6a85aa6c8473451f38f3148f
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-22 19:03:19 +1000

    Exception handling fixes in ImageSettings

--- TileCutter/ImageSettings.cs
+++ TileCutter/ImageSettings.cs
@@ -12,8 +12,8 @@ namespace TileCutter
         public ImageSettings(int zoom, int chunks, MagickImage image)
         {
             Zoom = zoom;
-            Chunks = chunks > 0 ? chunks : throw new ArgumentNullException($"{nameof(chunks)} can't be negative", nameof(chunks));
-            Image = image ?? throw new ArgumentOutOfRangeException($"{nameof(image)} can't be null", nameof(image));
+            Chunks = chunks > 0 ? chunks : throw new ArgumentOutOfRangeException($"{nameof(chunks)} can't be negative", nameof(chunks));
+            Image = image ?? throw new ArgumentNullException($"{nameof(image)} can't be null", nameof(image));
         }
     }
 }
