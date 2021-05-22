89cc06be3cb40ca1c8189c0b8cf8b720fe8c21d3
AlvinSeville7cf <alvinseville7cf@gmail.com>
2021-05-22 19:31:38 +1000

    Unnecessary usings removed

--- TileCutter/MainForm.cs
+++ TileCutter/MainForm.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
